811ec463375fed8a145cc84e0b0bb0feb42c703b
rafaelnicacio25 <rafaelnicacio@sempreceub.com>
2026-06-21 17:25:19 -0300

    Iniciando Projeto

---
+++ Parte_Inicial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int senha[4];
+    int palpite[4];
+    
+    printf("--- BEM-VINDO AO JOGO DA SENHA ---\n");
+    printf("Tente desbloquear o sistema adivinhando a senha de 5 digitos!\n");
+
+    return 0;
+}
